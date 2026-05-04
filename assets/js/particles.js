0ee64b191f8ed69597edaf180c2ce5ee8dac9352
NFNFeSports <nfnfes@gmail.com>
2026-05-04 18:25:39 +0100

    Update particles.js

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -1,1 +1,43 @@
+const canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
 
+const ctx = canvas.getContext("2d");
+canvas.style.position = "fixed";
+canvas.style.top = 0;
+canvas.style.left = 0;
+canvas.style.zIndex = 0;
+
+function resize(){
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+resize();
+window.onresize = resize;
+
+let particles = [];
+
+for(let i=0;i<80;i++){
+  particles.push({
+    x: Math.random()*canvas.width,
+    y: Math.random()*canvas.height,
+    dx:(Math.random()-0.5),
+    dy:(Math.random()-0.5)
+  });
+}
+
+function animate(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.fillStyle="#00fff7";
+
+  particles.forEach(p=>{
+    ctx.fillRect(p.x,p.y,2,2);
+    p.x+=p.dx;
+    p.y+=p.dy;
+
+    if(p.x<0||p.x>canvas.width) p.dx*=-1;
+    if(p.y<0||p.y>canvas.height) p.dy*=-1;
+  });
+
+  requestAnimationFrame(animate);
+}
+animate();
